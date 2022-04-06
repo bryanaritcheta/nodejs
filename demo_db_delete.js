b6942c39d7af86f49b10e8ddeffe77c3161d593a
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:19:35 +0800

    Update demo_db_delete.js

--- demo_db_delete.js
+++ demo_db_delete.js
@@ -13,6 +13,7 @@ con.connect(function(err) {
     var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
     con.query(sql, function(err, result) {
         if (err) throw err;
-        console.log("Number of records deleted: " + result.affectedRows);
+        // console.log("Number of records deleted: " + result.affectedRows);
+        console.log(result.affectedRows)
     });
 });
