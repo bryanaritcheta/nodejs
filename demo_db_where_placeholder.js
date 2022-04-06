c760d61b890bac5271617511e87d1464cdc394c9
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:15:37 +0800

    Create demo_db_where_placeholder.js

---
+++ demo_db_where_placeholder.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var adr = 'Mountain 21';
+    //Escape the address value:
+    var sql = 'SELECT * FROM customers WHERE address = ?';
+    //Send an array with value(s) to replace the escaped values:
+    con.query(sql, [adr], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
