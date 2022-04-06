02809fac9cfb0e98adf74ab94b2e9f05a371a7a2
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:25:21 +0800

    Create demo_db_update.js

---
+++ demo_db_update.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
+});
