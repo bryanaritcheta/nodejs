a4e033409ded079ca3d2a61c6a33a7949d5159cd
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:52:43 +0800

    Create demo_db_left_join.js

---
+++ demo_db_left_join.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "SELECT users.name AS user, products.name AS favorite FROM users LEFT JOIN products ON users.favorite_product = products.id";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
