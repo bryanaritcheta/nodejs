bd47d07fe1b1e90fa60051b7f8a9cc26fc817a0f
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:18:26 +0800

    Create demo_db_orderby.js

---
+++ demo_db_orderby.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers ORDER BY name", function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
