96afba0c7a3acc50646f3086c795a4fe517e8cec
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:15:35 +0800

    Create demo_db_where_escape.js

---
+++ demo_db_where_escape.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var adr = 'Mountain 21';
+    //Escape the address value:
+    var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
