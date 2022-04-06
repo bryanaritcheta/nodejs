cd17876faff647abf2f82a3fe19f0ada8a2be8d1
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:25:40 +0800

    Update demo_db_update.js

--- demo_db_update.js
+++ demo_db_update.js
@@ -13,6 +13,7 @@ con.connect(function(err) {
     var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
     con.query(sql, function(err, result) {
         if (err) throw err;
-        console.log(result.affectedRows + " record(s) updated");
+        // console.log(result.affectedRows + " record(s) updated");
+        console.log(result.affectedRows)
     });
 });
