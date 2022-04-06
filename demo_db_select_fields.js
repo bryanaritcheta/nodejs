821f4c3b1c512551a54e654745f32084f96a8edf
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:10:18 +0800

    Create demo_db_select_fields.js

---
+++ demo_db_select_fields.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT name, address FROM customers", function(err, result, fields) {
+        if (err) throw err;
+        console.log(fields);
+    });
+});
