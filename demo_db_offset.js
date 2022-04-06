3bab0cd71b4f55f59d65d591f212d68f9bd97322
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:27:25 +0800

    Create demo_db_offset.js

---
+++ demo_db_offset.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    //Return 5 customers, starting from position 2:
+    con.query("SELECT * FROM customers LIMIT 5 OFFSET 2", function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
