93daf33f28d5cdebb391551a7592fe4ee1451f1d
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-06 20:19:20 +0800

    Create demo_db_delete.js

---
+++ demo_db_delete.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.mysqluser,
+    password: process.env.mysqlpassword,
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Number of records deleted: " + result.affectedRows);
+    });
+});
