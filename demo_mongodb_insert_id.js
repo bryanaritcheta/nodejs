30e25aef98262dd960c2d53eeb922e661afc135b
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-08 23:41:03 +0800

    Create demo_mongodb_insert_id.js

---
+++ demo_mongodb_insert_id.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongousr = process.env.mongouser;
+const mongopw = process.env.mongopassword;
+const mongocluster = process.env.mongocluster;
+const mongodb = process.env.mongodb;
+
+const uri = `mongodb+srv://${mongousr}:${mongopw}@${mongocluster}/${mongodb}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = [
+        { _id: 154, name: 'Chocolate Heaven' },
+        { _id: 155, name: 'Tasty Lemon' },
+        { _id: 156, name: 'Vanilla Dream' }
+    ];
+    dbo.collection("products").insertMany(myobj, function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+    });
+});
