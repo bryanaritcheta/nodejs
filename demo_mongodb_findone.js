2f1d4bc8cf6035693cecf93f1a1c0cf98d0dce0e
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-08 23:48:09 +0800

    Create demo_mongodb_findone.js

---
+++ demo_mongodb_findone.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongousr = process.env.mongouser;
+const mongopw = process.env.mongopassword;
+const mongocluster = process.env.mongocluster;
+const mongodb = process.env.mongodb;
+
+const uri = `mongodb+srv://${mongousr}:${mongopw}@${mongocluster}/${mongodb}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection("customers").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result.name);
+        db.close();
+    });
+});
