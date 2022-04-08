c6798d397f36a58144edd674b1a83e97662f2cc2
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-08 23:40:59 +0800

    Create demo_mongodb_insert.js

---
+++ demo_mongodb_insert.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongousr = process.env.mongouser;
+const mongopw = process.env.mongopassword;
+const mongocluster = process.env.mongocluster;
+const mongodb = process.env.mongodb;
+
+const uri = `mongodb+srv://${mongousr}:${mongopw}@${mongocluster}/${mongodb}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+    });
+});
