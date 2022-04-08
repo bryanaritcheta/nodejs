bb938b97942861a39712f01a38eef12985998cf0
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-04-09 00:24:46 +0800

    Create demo_mongodb_join.js

---
+++ demo_mongodb_join.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongousr = process.env.mongouser;
+const mongopw = process.env.mongopassword;
+const mongocluster = process.env.mongocluster;
+const mongodb = process.env.mongodb;
+
+const uri = `mongodb+srv://${mongousr}:${mongopw}@${mongocluster}/${mongodb}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection('orders').aggregate([{
+        $lookup: {
+            from: 'products',
+            localField: 'product_id',
+            foreignField: '_id',
+            as: 'orderdetails'
+        }
+    }]).toArray(function(err, res) {
+        if (err) throw err;
+        console.log(JSON.stringify(res));
+        db.close();
+    });
+});
